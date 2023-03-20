31012f788cf48f85cbf054111ab1f2c6492f13b8
OJIMA-YUKIYA <ojima.yukiya@gmail.com>
2023-03-20 21:00:15 +0900

    update

---
+++ slice_edge.m
@@ -0,0 +1,13 @@
+function q = slice_edge(p1,p2,slice_X)
+    %SLICE_EDGE
+    % 辺 p1->p2 と断面 slice_X との交点 q を求めよ．
+    
+    
+    % y 座標を求めましょう
+    qy = 0; %　←　正しく書き換えましょう．
+    
+    % z 座標を求めましょう．
+    qz = 0; %　←　正しく書き換えましょう．
+    
+    q = [slice_X, qy, qz];
+end
