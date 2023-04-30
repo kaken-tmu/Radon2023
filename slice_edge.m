bd30208bba1d2aec9cca5812643b6ed8280a7c14
OJIMA-YUKIYA <ojima.yukiya@gmail.com>
2023-04-30 17:50:12 +0900

    update

--- slice_edge.m
+++ slice_edge.m
@@ -1,7 +1,10 @@
 function q = slice_edge(p1,p2,slice_X)
     %SLICE_EDGE
+    if p1(1) == p2(1)
+        error("slice_edge() error: 二点が同じです")
+    end
+   
     % 辺 p1->p2 と断面 slice_X との交点 q を求めよ．
-    
     
     % y 座標を求めましょう
     qy = 0; %　←　正しく書き換えましょう．
--- 正答/slice_edge.m
+++ 正答/slice_edge.m
@@ -1,13 +1,16 @@
 function q = slice_edge(p1,p2,slice_X)
     %SLICE_EDGE
+    if p1(1) == p2(1)
+        error("slice_edge() error: 二点が同じです")
+    end
+
     % 辺 p1->p2 と断面 slice_X との交点 q を求めよ．
-    
-    
+
     % y 座標を求めましょう
-    qy = 0; %　←　正しく書き換えましょう．
+    qy = (p2(2) - p1(2))/(p2(1) - p1(1)) * (slice_X - p1(1)) + p1(2); %　←　正しく書き換えましょう．
     
     % z 座標を求めましょう．
-    qz = 0; %　←　正しく書き換えましょう．
+    qz = (p2(3) - p1(3))/(p2(1) - p1(1)) * (slice_X - p1(1)) + p1(3); %　←　正しく書き換えましょう．
     
     q = [slice_X, qy, qz];
 end
